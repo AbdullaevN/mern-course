bd946d50bd8f0850a63f2b3083e14d2fffb14db7
nno <nurlanabdullaev9820@gmail.com>
2020-11-04 16:08:18 +0400

    1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mern-course
-react dev
+ react
---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from "./pages/routes"
+import 'materialize-css'
+
+function App() {
+    const routes = useRoutes(false )
+    return (
+      <Router>
+      <div className="container">
+          {routes}
+      </div>
+      </Router>
+  )
+}
+export default App
